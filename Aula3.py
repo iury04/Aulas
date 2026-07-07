2787cc2df6efff4aa0deb5fc6d8c65355b0fce7c
Iury Silva AC <iurytecnologia04@gmail.com>
2026-07-07 15:50:57 -0300

    Aula 3

---
+++ Aula3.py
@@ -0,0 +1,10 @@
+# nome = "Iury"
+# sobrenome = "Silva"
+# data = "04/09/2006"
+# # print(f"{nome} {sobrenome} nascido em {data}")
+
+nome = input("Escolha o nome do seu personagem: ")
+raca = input("Escolha a raça: EX(elfo, anão, humano, etc)")
+habilidade = input("Escolha a habilidade especial: ")
+
+print(f"{nome}, o(a) {raca}. possui a habilidade de {habilidade}. Uma nova aventura está prestes a começar!")

30ad31867b0e19a06d08b19db2baddb59e404361
Iury Silva AC <iurytecnologia04@gmail.com>
2026-07-07 15:16:15 -0300

    Aula 2

---
+++ Aula2.py
@@ -0,0 +1,12 @@
+# nome = input("Qual é seu nome? ")
+# print(f"Olá, {nome}! Seja bem-vindo(a)")
+
+# animal = input("Digite um animal: ")
+# lugar = input("Digite um lugar: ")
+# objeto = input("Digite um objeto: ")
+# print(f"Um {animal} foi até a {lugar} e encontrou um {objeto} mágico! ")
+
+ingrediente = input("Digite um ingrediente: ")
+utensilho = input("Digite um utensilho de cozinha: ")
+adjetivo = input("Digite um adjetivo: ")
+print(f"Com apenas {ingrediente}, um(a) {utensilho} e um toque de {adjetivo} você criou a receita mais inesperada do mundo! ")

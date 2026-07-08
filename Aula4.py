1ee331486dc7d840380a6d1f81ee99b550158845
Iury Silva AC <iurytecnologia04@gmail.com>
2026-07-08 12:46:32 -0300

    Aula 4

---
+++ Aula4.py
@@ -0,0 +1,16 @@
+# ano = int(input("Insira seu ano de nascimento: "))
+# conta = (2026 - ano)
+# print(f"Você tem {conta} anos!")
+
+
+# valor_compra = float(input("Qual é o valor da compra? "))
+# pagamento = float(input("Quanto o cliente pagou? "))
+# conta = (pagamento - valor_compra)
+# print(f"O valor do troco é: {conta}")
+
+
+n1 = int(input("Primeiro número: "))
+n2 = int(input("Segundo número: "))
+soma = (n1 + n2)
+
+print(f"O resultado da soma é: {soma}")
